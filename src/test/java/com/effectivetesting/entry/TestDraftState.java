8f8122eaaa68d8b248469cbab4522cb8eddae1c7
marcelo corpucci <mcorpucci@gmail.com>
2020-01-20 10:56:17 -0300

    draft entry with delete using rest

--- src/test/java/com/effectivetesting/entry/TestDraftState.java
+++ src/test/java/com/effectivetesting/entry/TestDraftState.java
@@ -1,30 +1,31 @@
 package com.effectivetesting.entry;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.delete;
 import static org.junit.Assert.assertFalse;
+
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.effectivetesting.pageobject.LoginPageObject;
+
+import io.restassured.path.json.JsonPath;
 
 public class TestDraftState {
 	private WebDriver driver;
 	private LoginPageObject loginPage;
+	private String DEFAULT_BASE_URL = "http://localhost:5000";
 	
 	@Before
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("localhost:5000");
+		driver.get(DEFAULT_BASE_URL);
 		loginPage = new LoginPageObject(driver);
 	}
 	
@@ -43,25 +44,18 @@ public class TestDraftState {
 	
 	@After
 	public void teardDown() {
-//		driver.get("http://localhost:5000");
-//		driver.findElement(By.id("login")).click();
-//		driver.findElement(By.id("email")).sendKeys("admin1@gmail.com");
-//		driver.findElement(By.id("password")).sendKeys("admin1");
-//		driver.findElement(By.id("btn-submit")).click();
-//		
-//		driver.get("http://localhost:5000/admin/entry/");
-//		
-//		driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/form")).click();
-//		
-//	    WebDriverWait wait = new WebDriverWait(driver, 10);
-//	    Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-//		   
-//	    alert.accept();
-//	    
-//	    WebDriverWait waitForMessage = new WebDriverWait(driver, 10);
-//	    waitForMessage.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/div[2]")));
-//	    
-//	    driver.quit();
+		JsonPath json = given()
+							.contentType("application/json")
+						.when()
+							.get(DEFAULT_BASE_URL + "/api/entry")
+						.thenReturn()
+							.jsonPath();
+		
+		List<Integer> entryIds = json.getJsonObject("objects.id");
+
+		delete(DEFAULT_BASE_URL + "/api/entry/" + entryIds.get(entryIds.size() - 1));
+		
+		driver.quit();
 	}
 
 }
--- src/test/java/com/effectivetesting/sanity/TestLogin.java
+++ src/test/java/com/effectivetesting/sanity/TestLogin.java
@@ -1,6 +1,6 @@
 package com.effectivetesting.sanity;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
